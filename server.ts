46e7f6959b43039311330f214afb2d125e090250
bayek0fsiwa <imvikas@tutanota.com>
2024-05-06 17:53:01 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import app from "./src/app";
+import { config } from "./src/configs/config";
 
 
 const startServer = () => {
-    const port = process.env.PORT || 8080;
+    const port = config.port || 8080;
 
     app.listen(port, () => {
         console.log(`Running on http://localhost:${port}`);
---
+++ src/configs/config.ts
@@ -0,0 +1,12 @@
+import { config as conf } from "dotenv";
+
+// dotenv config init
+conf()
+
+
+const _config = {
+    port: process.env.PORT
+}
+
+
+export const config = Object.freeze(_config);
