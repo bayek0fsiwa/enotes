0691219265ca1169b0cedbabbb106fcccd1f4877
Bayek0fsiwa <imvikas@tutanota.com>
2024-05-07 17:43:04 +0530

    add files uploads

--- src/notes/notesController.ts
+++ src/notes/notesController.ts
@@ -2,5 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 
 export const addNote = async (req: Request, res: Response, next: NextFunction) => {
+    console.log("file", req.files)
     res.status(200).json({ message: "OK" })
 }
