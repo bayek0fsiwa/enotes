a6699073b02f08cbc3e1d534ba29fe6b56bc0b97
Bayek0fsiwa <imvikas@tutanota.com>
2024-05-07 17:35:13 +0530

    add multer

--- src/notes/notesRouter.ts
+++ src/notes/notesRouter.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import { addNote } from "./notesController";
+import multer from "multer";
+import path from "path";
 
 const userRouter = express.Router();
 
-userRouter.post("/add", addNote);
+const upload = multer({
+    dest: path.resolve(__dirname, "../../public/data/uploads"),
+    limits: { fileSize: 3e7 }, // 30 MB
+
+});
+
+userRouter.post("/add", upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+]), addNote);
 
 export default userRouter;
