0115f8139b0c6192987f94918facf76d0a7f42e0
bayek0fsiwa <imvikas@tutanota.com>
2024-08-26 12:28:33 +0530

    add patch endpoint

--- src/note/noteController.ts
+++ src/note/noteController.ts
@@ -48,3 +48,66 @@ export const addNote = async (req: Request, res: Response, next: NextFunction) =
         return next(createHttpError(500, "Error while uploading file!"));
     }
 }
+
+
+export const updateNote = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { title, genre } = req.body
+        const noteId = req.params.noteId
+
+        const note = await noteModel.findOne({ _id: noteId })
+
+        if (!note) {
+            return next(createHttpError(404, "Note not found"))
+        }
+
+        const _req = req as AuthRequest
+
+        if (note.author.toString() !== _req.userId) {
+            return next(createHttpError(403, "Unauthorized"))
+        }
+
+        const files = req.files as { [fieldname: string]: Express.Multer.File[] }
+        let completeCoverImage = ""
+
+        if (files.coverImage) {
+            const filename = files.coverImage[0].filename
+            const coverMimeType = files.coverImage[0].mimetype.split("/").at(-1)
+            const filePath = path.resolve(__dirname, "../../public/data/uploads" + filename)
+            completeCoverImage = filename
+            const uploadResult = await cloudinary.uploader.upload(filePath, {
+                filename_override: completeCoverImage,
+                folder: "note-covers",
+                format: coverMimeType
+            })
+
+            completeCoverImage = uploadResult.secure_url
+            await fs.promises.unlink(filePath)
+        }
+
+        let completeFileName = ""
+
+        if (files.file) {
+            const noteFilePath = path.resolve(__dirname, "../../public/data/uploads" + files.file[0].filename)
+            const noteFileName = files.file[0].filename
+            completeFileName = noteFileName
+
+            const uploadResultPdf = await cloudinary.uploader.upload(noteFilePath, {
+                resource_type: "raw",
+                filename_override: completeFileName,
+                folder: "note-pdfs",
+                format: "pdf"
+            })
+
+            completeFileName = uploadResultPdf.secure_url
+            await fs.promises.unlink(noteFilePath)
+        }
+
+        const updateNote = await noteModel.findOneAndUpdate({ _id: noteId }, { title: title, genre: genre, coverImage: completeCoverImage ? completeCoverImage : note.coverImage, file: completeFileName ? completeFileName : note.file }, { new: true })
+        res.json(updateNote)
+
+    } catch (err) {
+        next(createHttpError(500, "cannot update note!"))
+    }
+
+}
